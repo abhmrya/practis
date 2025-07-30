b8f5fe6ec07831d410993744985caa905d7bac2e
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-07-30 15:37:17 +0530

    commit

--- ecommerce/ecommerceapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('',views.index,name="index"),
-    path('contact/',views.contact,name="contact"),
-     path('about/',views.about,name="about"),
-]
--- ecommerce/my_auth/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    number = models.CharField(max_length=15, null=True, blank=True)
