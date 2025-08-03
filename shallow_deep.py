3f7cef6c87aa39cfdfb2d3bb53ca5f58da7e0d0a
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-08-03 11:26:11 +0530

    shallow_deep

--- shallow_deep.py
+++ shallow_deep.py
@@ -5,17 +5,29 @@ a=[1,2,3,[4,5,6]]
 
 s=copy.copy(a)
 s[3][0]=7
-print(a)
-print(s)
+# print(a)
+# print(s)
 
 # deep copy
 c=[1,2,3,[4,5,6],7]
 d=copy.deepcopy(c)
 d[3][0]=7
-print(c)
-print(d)
+# print(c)
+# print(d)
 
 e=copy.deepcopy(c)
 e[0]=8
-print(c)
-print(e)
+# print(c)
+# print(e)
+
+f=[1,2,3,4,5,6,7]
+g=copy.deepcopy(f)
+g[3]=5
+# print(f)
+# print(g)
+
+h=[1,2,3,[4,5,[6,7,8,9]]]
+i=copy.deepcopy(h)
+i[3][2][0]=10
+# print(h)
+# print(i)
