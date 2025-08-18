d29c617c2aadcf405eb4dcdcf05eced421828480
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-08-18 21:37:10 +0530

    last_to_first

--- python/last_to_first_item.py
+++ python/last_to_first_item.py
@@ -1,9 +1,28 @@
 lst=['ab','cd','ef','gh']
 
 print(type(lst))
-lst.remove(-1)
-print(lst)
+last=lst.pop(-1)
 print(last)
-print(type(last))
-newlist=lst.append(last)
-print(newlist)
+print(lst)
+print([last]+lst)
+
+
+lst=['ab','cd','ef','gh']
+print(lst)
+last=lst.pop(-1)
+lst.insert(0, last)
+print(lst)
+
+
+# remove 20 and insert 25
+lst = [10, 20, 30, 40, 50]
+last = lst.pop(1)
+print(last)
+lst.insert(1,25)
+print(lst)
+
+lst = [10, 20, 30, 40, 50]
+element=25
+for i in lst:
+    if i==30:
+        lst.insert()
